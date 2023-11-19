8381fa59a29807cf68eeb89d1e9096535ccdedd8
Sunyvrm1 <verma.suny1603@gmail.com>
2023-11-19 17:04:13 +0530

    head_revise

--- dist/js/script.js
+++ dist/js/script.js
@@ -4,15 +4,3 @@ const headerCont = document.querySelector(".headerCont");
 navIcon.addEventListener("click", () => {
   headerCont.classList.toggle("active");
 });
-
-//Change height on scroll
-
-function changeHeader() {
-  let navbarHeight = document.querySelector(".headerCont");
-  if (document.documentElement.scrollTop < 110) {
-    navbarHeight.classList.remove("shrink");
-  } else {
-    navbarHeight.classList.add("shrink");
-  }
-}
-window.addEventListener("scroll", changeHeader);

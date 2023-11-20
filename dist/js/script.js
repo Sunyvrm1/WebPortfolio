a69637dae16d89c4cc22ecebb9e0a110bf35d758
Sunyvrm1 <115399549+Sunyvrm1@users.noreply.github.com>
2023-11-20 10:29:22 +0000

    changes1

--- dist/js/script.js
+++ dist/js/script.js
@@ -4,3 +4,27 @@ const headerCont = document.querySelector(".headerCont");
 navIcon.addEventListener("click", () => {
   headerCont.classList.toggle("active");
 });
+
+
+const skillSection = document.getElementById('skill');
+
+function animateSkillsOnScroll() {
+  function checkScroll() {
+    const rect = skillSection.getBoundingClientRect();
+    const windowHeight = window.innerHeight || document.documentElement.clientHeight;
+
+    if (rect.top < windowHeight && rect.bottom >= 0) {
+      skillSection.classList.add('skill-animation');
+      window.removeEventListener('scroll', checkScroll);
+    }
+  }
+
+  window.addEventListener('scroll', checkScroll);
+}
+
+animateSkillsOnScroll(); // Trigger the scroll animation on initial load
+
+
+window.addEventListener('scroll', function() {
+  console.log(window.scrollY);
+});

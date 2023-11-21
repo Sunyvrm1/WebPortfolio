4e986625d3451acd7d9096440f651d389ff6c18c
Sunyvrm1 <115399549+Sunyvrm1@users.noreply.github.com>
2023-11-21 12:17:10 +0000

    21nov

--- dist/js/script.js
+++ dist/js/script.js
@@ -5,26 +5,24 @@ navIcon.addEventListener("click", () => {
   headerCont.classList.toggle("active");
 });
 
+// const skillSection = document.getElementById('skill');
 
-const skillSection = document.getElementById('skill');
+// function animateSkillsOnScroll() {
+//   function checkScroll() {
+//     const rect = skillSection.getBoundingClientRect();
+//     const windowHeight = window.innerHeight || document.documentElement.clientHeight;
 
-function animateSkillsOnScroll() {
-  function checkScroll() {
-    const rect = skillSection.getBoundingClientRect();
-    const windowHeight = window.innerHeight || document.documentElement.clientHeight;
+//     if (rect.top < windowHeight && rect.bottom >= 0) {
+//       skillSection.classList.add('skill-animation');
+//       window.removeEventListener('scroll', checkScroll);
+//     }
+//   }
 
-    if (rect.top < windowHeight && rect.bottom >= 0) {
-      skillSection.classList.add('skill-animation');
-      window.removeEventListener('scroll', checkScroll);
-    }
-  }
+//   window.addEventListener('scroll', checkScroll);
+// }
 
-  window.addEventListener('scroll', checkScroll);
-}
+// animateSkillsOnScroll(); // Trigger the scroll animation on initial load
 
-animateSkillsOnScroll(); // Trigger the scroll animation on initial load
-
-
-window.addEventListener('scroll', function() {
-  console.log(window.scrollY);
-});
+// window.addEventListener('scroll', function() {
+//   console.log(window.scrollY);
+// });
